02250ed4170985607780132534d23ac6309fbac7
cheininger <christoph_heininger@hotmail.com>
2023-11-19 15:32:00 +0100

    basic JS functions

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(name, author, pages, read) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(name, author, pages, read) {
+  newBook = new Book(name, author, pages, read);
+
+  myLibrary.push(newBook);
+}
